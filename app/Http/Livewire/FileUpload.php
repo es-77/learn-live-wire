9a7a7e26e3d065b5c414a385e5a14720dbcc3f53
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2024-03-01 23:37:33 +0500

    file upload

--- app/Http/Livewire/FileUpload.php
+++ app/Http/Livewire/FileUpload.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class FileUpload extends Component
 {
+    use WithFileUploads;
+
+    public $name, $email, $password, $image, $filePath;
+    public function createUser()
+    {
+        $this->validate([
+            'name' => ['required'],
+            'email' => [
+                'required',
+                'email',
+                'unique:users'
+            ],
+            'password' => ['required'],
+            'image' => ['required']
+        ]);
+        if ($this->image) {
+            $this->filePath = $this->image->store('upload', 'public');
+        }
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'file' => $this->filePath,
+            'password' => bcrypt($this->password)
+        ]);
+    }
     public function render()
     {
         return view('livewire.file-upload');
